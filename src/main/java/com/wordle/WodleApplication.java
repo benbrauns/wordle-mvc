9dfc3e1967c8faa552ac0e15eaa6fdcda4b482d4
Ben Brauns <braunsben@gmail.com>
2022-11-16 07:58:12 -0600

    initial commit

---
+++ src/main/java/com/wordle/WodleApplication.java
@@ -0,0 +1,13 @@
+package com.wordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WodleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordle/WodleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
